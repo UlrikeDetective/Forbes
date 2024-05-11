b9efb2f873fcb24420fb489dcaf5ee046c918d5d
[Ulrike Herold] <[herold.ulrike@gmail.com]>
2024-05-11 21:54:44 +0200

    new queries

--- forbes_sql/forbes_queries_new.sql
+++ forbes_sql/forbes_queries_new.sql
@@ -1,6 +1,6 @@
 Use Forbes_new;
 
-SELECT forbes_names.Person, forbes_names.ID, forbes_2022.net_worth_in_BillionUSD_2022
+SELECT forbes_names.Person, forbes_names.ID, forbes_names.Country_of_residence, forbes_2022.net_worth_in_BillionUSD_2022
 FROM forbes_names
 LEFT JOIN forbes_2022 ON forbes_names.ID = forbes_2022.USER_ID
 ORDER BY forbes_2022.net_worth_in_BillionUSD_2022 DESC;
@@ -99,3 +99,277 @@ FROM
     left join
     forbes_location on forbes_names.City = forbes_location.City
 ORDER BY forbes_names.ID;
+
+SELECT 
+    forbes_names.Country_of_residence,
+    SUM(forbes_2022.net_worth_in_BillionUSD_2022) AS total_net_worth
+FROM 
+    forbes_names
+LEFT JOIN 
+    forbes_2022 ON forbes_names.ID = forbes_2022.USER_ID
+GROUP BY 
+    forbes_names.Country_of_residence
+ORDER BY 
+    total_net_worth DESC;
+    
+SELECT 
+    forbes_names.Country_of_residence,
+    SUM(forbes_2022.net_worth_in_BillionUSD_2022) AS 2022_total,
+    SUM(forbes_2023.net_worth_in_BillionUSD_2023) AS 2023_total
+FROM 
+    forbes_names
+LEFT JOIN 
+    forbes_2022 ON forbes_names.ID = forbes_2022.USER_ID
+LEFT JOIN 
+    forbes_2023 ON forbes_names.ID = forbes_2023.USER_ID
+GROUP BY 
+    forbes_names.Country_of_residence
+ORDER BY 
+    2022_total DESC;
+
+SELECT 
+    forbes_names.Country_of_residence,
+    SUM(forbes_2022.net_worth_in_BillionUSD_2022) AS 2022_total,
+    SUM(forbes_2023.net_worth_in_BillionUSD_2023) AS 2023_total,
+    SUM(forbes_2024_01.net_worth_in_BillionUSD_2024_01) AS 2024_01_total,
+    SUM(forbes_2024_02.net_worth_in_BillionUSD_2024_02) AS 2024_02_total,
+    SUM(forbes_2024_03.net_worth_in_BillionUSD_2024_03) AS 2024_03_total,
+    SUM(forbes_2024_04.net_worth_in_BillionUSD_2024_04) AS 2024_04_total
+FROM 
+    forbes_names
+LEFT JOIN 
+    forbes_2022 ON forbes_names.ID = forbes_2022.USER_ID
+LEFT JOIN 
+    forbes_2023 ON forbes_names.ID = forbes_2023.USER_ID
+LEFT JOIN 
+    forbes_2024_01 ON forbes_names.ID = forbes_2024_01.USER_ID
+LEFT JOIN 
+    forbes_2024_02 ON forbes_names.ID = forbes_2024_02.USER_ID
+LEFT JOIN 
+    forbes_2024_03 ON forbes_names.ID = forbes_2024_03.USER_ID
+LEFT JOIN 
+    forbes_2024_04 ON forbes_names.ID = forbes_2024_04.USER_ID
+GROUP BY 
+    forbes_names.Country_of_residence
+ORDER BY 
+    2022_total DESC;
+    
+SELECT 
+    forbes_names.Continent,
+    SUM(forbes_2022.net_worth_in_BillionUSD_2022) AS 2022_total,
+    SUM(forbes_2023.net_worth_in_BillionUSD_2023) AS 2023_total,
+    SUM(forbes_2024_01.net_worth_in_BillionUSD_2024_01) AS 2024_01_total,
+    SUM(forbes_2024_02.net_worth_in_BillionUSD_2024_02) AS 2024_02_total,
+    SUM(forbes_2024_03.net_worth_in_BillionUSD_2024_03) AS 2024_03_total,
+    SUM(forbes_2024_04.net_worth_in_BillionUSD_2024_04) AS 2024_04_total
+FROM 
+    forbes_names
+LEFT JOIN 
+    forbes_2022 ON forbes_names.ID = forbes_2022.USER_ID
+LEFT JOIN 
+    forbes_2023 ON forbes_names.ID = forbes_2023.USER_ID
+LEFT JOIN 
+    forbes_2024_01 ON forbes_names.ID = forbes_2024_01.USER_ID
+LEFT JOIN 
+    forbes_2024_02 ON forbes_names.ID = forbes_2024_02.USER_ID
+LEFT JOIN 
+    forbes_2024_03 ON forbes_names.ID = forbes_2024_03.USER_ID
+LEFT JOIN 
+    forbes_2024_04 ON forbes_names.ID = forbes_2024_04.USER_ID
+GROUP BY 
+    forbes_names.Continent
+ORDER BY 
+    2022_total DESC;
+
+
+SELECT 
+    forbes_names.Continent,
+    forbes_names.Country_of_residence,
+    SUM(forbes_2022.net_worth_in_BillionUSD_2022) AS 2022_total,
+    SUM(forbes_2023.net_worth_in_BillionUSD_2023) AS 2023_total,
+    SUM(forbes_2024_01.net_worth_in_BillionUSD_2024_01) AS 2024_01_total,
+    SUM(forbes_2024_02.net_worth_in_BillionUSD_2024_02) AS 2024_02_total,
+    SUM(forbes_2024_03.net_worth_in_BillionUSD_2024_03) AS 2024_03_total,
+    SUM(forbes_2024_04.net_worth_in_BillionUSD_2024_04) AS 2024_04_total
+FROM 
+    forbes_names
+LEFT JOIN 
+    forbes_2022 ON forbes_names.ID = forbes_2022.USER_ID
+LEFT JOIN 
+    forbes_2023 ON forbes_names.ID = forbes_2023.USER_ID
+LEFT JOIN 
+    forbes_2024_01 ON forbes_names.ID = forbes_2024_01.USER_ID
+LEFT JOIN 
+    forbes_2024_02 ON forbes_names.ID = forbes_2024_02.USER_ID
+LEFT JOIN 
+    forbes_2024_03 ON forbes_names.ID = forbes_2024_03.USER_ID
+LEFT JOIN 
+    forbes_2024_04 ON forbes_names.ID = forbes_2024_04.USER_ID
+GROUP BY 
+    forbes_names.Continent, forbes_names.Country_of_residence
+ORDER BY 
+    2022_total DESC;
+
+SELECT 
+    forbes_names.City,
+    SUM(forbes_2022.net_worth_in_BillionUSD_2022) AS 2022_total,
+    SUM(forbes_2023.net_worth_in_BillionUSD_2023) AS 2023_total,
+    SUM(forbes_2024_01.net_worth_in_BillionUSD_2024_01) AS 2024_01_total,
+    SUM(forbes_2024_02.net_worth_in_BillionUSD_2024_02) AS 2024_02_total,
+    SUM(forbes_2024_03.net_worth_in_BillionUSD_2024_03) AS 2024_03_total,
+    SUM(forbes_2024_04.net_worth_in_BillionUSD_2024_04) AS 2024_04_total
+FROM 
+    forbes_names
+LEFT JOIN 
+    forbes_2022 ON forbes_names.ID = forbes_2022.USER_ID
+LEFT JOIN 
+    forbes_2023 ON forbes_names.ID = forbes_2023.USER_ID
+LEFT JOIN 
+    forbes_2024_01 ON forbes_names.ID = forbes_2024_01.USER_ID
+LEFT JOIN 
+    forbes_2024_02 ON forbes_names.ID = forbes_2024_02.USER_ID
+LEFT JOIN 
+    forbes_2024_03 ON forbes_names.ID = forbes_2024_03.USER_ID
+LEFT JOIN 
+    forbes_2024_04 ON forbes_names.ID = forbes_2024_04.USER_ID
+GROUP BY 
+    forbes_names.City
+ORDER BY 
+    2022_total DESC;
+
+SELECT 
+    forbes_names.Continent,
+    forbes_names.Country_of_residence,
+    forbes_names.City,
+    SUM(forbes_2022.net_worth_in_BillionUSD_2022) AS 2022_total,
+    SUM(forbes_2023.net_worth_in_BillionUSD_2023) AS 2023_total,
+    SUM(forbes_2024_01.net_worth_in_BillionUSD_2024_01) AS 2024_01_total,
+    SUM(forbes_2024_02.net_worth_in_BillionUSD_2024_02) AS 2024_02_total,
+    SUM(forbes_2024_03.net_worth_in_BillionUSD_2024_03) AS 2024_03_total,
+    SUM(forbes_2024_04.net_worth_in_BillionUSD_2024_04) AS 2024_04_total
+FROM 
+    forbes_names
+LEFT JOIN 
+    forbes_2022 ON forbes_names.ID = forbes_2022.USER_ID
+LEFT JOIN 
+    forbes_2023 ON forbes_names.ID = forbes_2023.USER_ID
+LEFT JOIN 
+    forbes_2024_01 ON forbes_names.ID = forbes_2024_01.USER_ID
+LEFT JOIN 
+    forbes_2024_02 ON forbes_names.ID = forbes_2024_02.USER_ID
+LEFT JOIN 
+    forbes_2024_03 ON forbes_names.ID = forbes_2024_03.USER_ID
+LEFT JOIN 
+    forbes_2024_04 ON forbes_names.ID = forbes_2024_04.USER_ID
+GROUP BY 
+    forbes_names.Continent, forbes_names.Country_of_residence, forbes_names.City
+ORDER BY 
+    2022_total DESC;
+
+SELECT 
+    forbes_names.Gender,
+    SUM(forbes_2022.net_worth_in_BillionUSD_2022) AS 2022_total,
+    SUM(forbes_2023.net_worth_in_BillionUSD_2023) AS 2023_total,
+    SUM(forbes_2024_01.net_worth_in_BillionUSD_2024_01) AS 2024_01_total,
+    SUM(forbes_2024_02.net_worth_in_BillionUSD_2024_02) AS 2024_02_total,
+    SUM(forbes_2024_03.net_worth_in_BillionUSD_2024_03) AS 2024_03_total,
+    SUM(forbes_2024_04.net_worth_in_BillionUSD_2024_04) AS 2024_04_total
+FROM 
+    forbes_names
+LEFT JOIN 
+    forbes_2022 ON forbes_names.ID = forbes_2022.USER_ID
+LEFT JOIN 
+    forbes_2023 ON forbes_names.ID = forbes_2023.USER_ID
+LEFT JOIN 
+    forbes_2024_01 ON forbes_names.ID = forbes_2024_01.USER_ID
+LEFT JOIN 
+    forbes_2024_02 ON forbes_names.ID = forbes_2024_02.USER_ID
+LEFT JOIN 
+    forbes_2024_03 ON forbes_names.ID = forbes_2024_03.USER_ID
+LEFT JOIN 
+    forbes_2024_04 ON forbes_names.ID = forbes_2024_04.USER_ID
+GROUP BY 
+    forbes_names.Gender
+ORDER BY 
+    2022_total DESC;
+
+SELECT 
+    forbes_names.Self_made,
+    SUM(forbes_2022.net_worth_in_BillionUSD_2022) AS 2022_total,
+    SUM(forbes_2023.net_worth_in_BillionUSD_2023) AS 2023_total,
+    SUM(forbes_2024_01.net_worth_in_BillionUSD_2024_01) AS 2024_01_total,
+    SUM(forbes_2024_02.net_worth_in_BillionUSD_2024_02) AS 2024_02_total,
+    SUM(forbes_2024_03.net_worth_in_BillionUSD_2024_03) AS 2024_03_total,
+    SUM(forbes_2024_04.net_worth_in_BillionUSD_2024_04) AS 2024_04_total
+FROM 
+    forbes_names
+LEFT JOIN 
+    forbes_2022 ON forbes_names.ID = forbes_2022.USER_ID
+LEFT JOIN 
+    forbes_2023 ON forbes_names.ID = forbes_2023.USER_ID
+LEFT JOIN 
+    forbes_2024_01 ON forbes_names.ID = forbes_2024_01.USER_ID
+LEFT JOIN 
+    forbes_2024_02 ON forbes_names.ID = forbes_2024_02.USER_ID
+LEFT JOIN 
+    forbes_2024_03 ON forbes_names.ID = forbes_2024_03.USER_ID
+LEFT JOIN 
+    forbes_2024_04 ON forbes_names.ID = forbes_2024_04.USER_ID
+GROUP BY 
+    forbes_names.Self_made
+ORDER BY 
+    2022_total DESC;
+
+SELECT 
+    forbes_names.Business,
+    SUM(forbes_2022.net_worth_in_BillionUSD_2022) AS 2022_total,
+    SUM(forbes_2023.net_worth_in_BillionUSD_2023) AS 2023_total,
+    SUM(forbes_2024_01.net_worth_in_BillionUSD_2024_01) AS 2024_01_total,
+    SUM(forbes_2024_02.net_worth_in_BillionUSD_2024_02) AS 2024_02_total,
+    SUM(forbes_2024_03.net_worth_in_BillionUSD_2024_03) AS 2024_03_total,
+    SUM(forbes_2024_04.net_worth_in_BillionUSD_2024_04) AS 2024_04_total
+FROM 
+    forbes_names
+LEFT JOIN 
+    forbes_2022 ON forbes_names.ID = forbes_2022.USER_ID
+LEFT JOIN 
+    forbes_2023 ON forbes_names.ID = forbes_2023.USER_ID
+LEFT JOIN 
+    forbes_2024_01 ON forbes_names.ID = forbes_2024_01.USER_ID
+LEFT JOIN 
+    forbes_2024_02 ON forbes_names.ID = forbes_2024_02.USER_ID
+LEFT JOIN 
+    forbes_2024_03 ON forbes_names.ID = forbes_2024_03.USER_ID
+LEFT JOIN 
+    forbes_2024_04 ON forbes_names.ID = forbes_2024_04.USER_ID
+GROUP BY 
+    forbes_names.Business
+ORDER BY 
+    2022_total DESC;
+
+SELECT 
+    forbes_names.Industry,
+    SUM(forbes_2022.net_worth_in_BillionUSD_2022) AS 2022_total,
+    SUM(forbes_2023.net_worth_in_BillionUSD_2023) AS 2023_total,
+    SUM(forbes_2024_01.net_worth_in_BillionUSD_2024_01) AS 2024_01_total,
+    SUM(forbes_2024_02.net_worth_in_BillionUSD_2024_02) AS 2024_02_total,
+    SUM(forbes_2024_03.net_worth_in_BillionUSD_2024_03) AS 2024_03_total,
+    SUM(forbes_2024_04.net_worth_in_BillionUSD_2024_04) AS 2024_04_total
+FROM 
+    forbes_names
+LEFT JOIN 
+    forbes_2022 ON forbes_names.ID = forbes_2022.USER_ID
+LEFT JOIN 
+    forbes_2023 ON forbes_names.ID = forbes_2023.USER_ID
+LEFT JOIN 
+    forbes_2024_01 ON forbes_names.ID = forbes_2024_01.USER_ID
+LEFT JOIN 
+    forbes_2024_02 ON forbes_names.ID = forbes_2024_02.USER_ID
+LEFT JOIN 
+    forbes_2024_03 ON forbes_names.ID = forbes_2024_03.USER_ID
+LEFT JOIN 
+    forbes_2024_04 ON forbes_names.ID = forbes_2024_04.USER_ID
+GROUP BY 
+    forbes_names.Industry
+ORDER BY 
+    2022_total DESC;
