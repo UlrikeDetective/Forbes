26edaedc6ec0259db0989233a869e0ff1e097550
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2024-08-22 20:48:54 +0200

    SQL Data Agg

---
+++ forbes_sql/Forbes_data_agg.sql
@@ -0,0 +1,28 @@
+Use Forbes;
+
+SELECT SUM(Net_worth_in_BillionUSD_2024_03) AS total_net_worth_2024_03
+FROM forbes_2024_03;
+
+SELECT SUM(Net_worth_in_BillionUSD_2024_03) AS total_net_worth_2024_03
+FROM forbes_2024_03
+Where Age_of_person_2024_03 = 60;
+
+SELECT AVG(Net_worth_in_BillionUSD_2024_03) AS AVG_net_worth_2024_03
+FROM forbes_2024_03;
+
+SELECT COUNT(Net_worth_in_BillionUSD_2024_03) AS Count_net_worth_2024_03
+FROM forbes_2024_03;
+
+SELECT Max(Net_worth_in_BillionUSD_2024_03) AS max_net_worth_2024_03
+FROM forbes_2024_03;
+
+SELECT Min(Net_worth_in_BillionUSD_2024_03) AS min_net_worth_2024_03
+FROM forbes_2024_03;
+
+SELECT MAX(Net_worth_in_BillionUSD_2024_03) as highest_networth, MIN(Net_worth_in_BillionUSD_2024_03) as lowest_networth
+FROM forbes_2024_03;
+
+SELECT
+COUNT(User_ID) AS number_of_UserID,
+SUM(Net_worth_in_BillionUSD_2024_03) AS total_sum
+FROM forbes_2024_03;
