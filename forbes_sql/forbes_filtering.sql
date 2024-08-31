4250a266f45708ba3d594e021f8e351992009863
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2024-08-31 09:59:07 +0200

    Forbes August 2024 raw

--- forbes_sql/forbes_filtering.sql
+++ forbes_sql/forbes_filtering.sql
@@ -7,3 +7,8 @@ WHERE Net_worth_in_BillionUSD_2022 > 75 AND Age_of_person_2022 < 50;
 SELECT User_ID, Person, Age_of_person_2022
 FROM forbes_2022
 WHERE Net_worth_in_BillionUSD_2022 > 75 OR Age_of_person_2022 < 50;
+
+SELECT Country_of_residence, COUNT(*) AS itemCount
+FROM forbes_names
+GROUP BY Country_of_residence
+HAVING  itemCount > 25;
