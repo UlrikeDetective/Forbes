50e3ce73c4fb0e8b52dd3922727662208e2f2358
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2024-09-02 21:11:48 +0200

    Update forbes_union.sql

--- Forbes/forbes_sql/forbes_union.sql
+++ Forbes/forbes_sql/forbes_union.sql
@@ -3,3 +3,7 @@ Use Forbes;
 SELECT Person, Net_worth_in_BillionUSD_2022 FROM forbes_2022
 UNION
 SELECT Person, Net_worth_in_BillionUSD_2023 FROM forbes_2023;
+
+SELECT Person, Net_worth_in_BillionUSD_2024_07 FROM forbes_2024_07
+UNION
+SELECT Person, Net_worth_in_BillionUSD_2024_08 FROM forbes_2024_08;
