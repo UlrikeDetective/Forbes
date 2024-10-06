80c0bda64164f5c6d9aed0dd4a5d68be257fe34a
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2024-10-06 08:46:34 +0200

    Update forbes_union.sql

--- Forbes/forbes_sql/forbes_union.sql
+++ Forbes/forbes_sql/forbes_union.sql
@@ -7,3 +7,7 @@ SELECT Person, Net_worth_in_BillionUSD_2023 FROM forbes_2023;
 SELECT Person, Net_worth_in_BillionUSD_2024_07 FROM forbes_2024_07
 UNION
 SELECT Person, Net_worth_in_BillionUSD_2024_08 FROM forbes_2024_08;
+
+SELECT Person, Net_worth_in_BillionUSD_2024_08 FROM forbes_2024_08
+UNION
+SELECT Person, Net_worth_in_BillionUSD_2024_09 FROM forbes_2024_09;
