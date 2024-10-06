2a30aa037ab6164145e3c16bedcf201e8a3022ab
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2024-10-06 08:49:25 +0200

    Update forbes_filtering.sql

--- Forbes/forbes_sql/forbes_filtering.sql
+++ Forbes/forbes_sql/forbes_filtering.sql
@@ -4,6 +4,10 @@ SELECT User_ID, Person, Age_of_person_2022
 FROM forbes_2022
 WHERE Net_worth_in_BillionUSD_2022 > 75 AND Age_of_person_2022 < 50;
 
+SELECT User_ID, Person, Age_of_person_2024_08
+FROM forbes_2024_08
+WHERE Net_worth_in_BillionUSD_2024_08 > 75 AND Age_of_person_2024_08 < 50;
+
 SELECT User_ID, Person, Age_of_person_2022
 FROM forbes_2022
 WHERE Net_worth_in_BillionUSD_2022 > 75 OR Age_of_person_2022 < 50;
@@ -16,4 +20,9 @@ HAVING  itemCount > 25;
 SELECT City COUNT(*) AS itemCount
 FROM forbes_names
 GROUP BY City
+HAVING  itemCount > 15;
+
+SELECT Citzenship COUNT(*) AS itemCount
+FROM forbes_names
+GROUP BY Citzenship
 HAVING  itemCount > 15;
