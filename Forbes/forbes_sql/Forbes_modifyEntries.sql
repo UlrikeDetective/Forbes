93b5c9d80916f6120cfec549f6157f5deae497f0
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2024-11-03 22:04:34 +0100

    Update Forbes_modifyEntries.sql

--- Forbes/forbes_sql/Forbes_modifyEntries.sql
+++ Forbes/forbes_sql/Forbes_modifyEntries.sql
@@ -1,7 +1,7 @@
 Use Forbes;
 
 SELECT * FROM forbes_names WHERE Person = 'Taylor Swift';
-SELECT * FROM forbes_names WHERE Person = 'Elon Musk';
+SELECT * FROM forbes_names WHERE Person = 'Susanne Klatten';
 
 UPDATE forbes_names
 SET City = 'Nashville', State = 'Tennessee', Citzenship = 'US', Self_made = 'TRUE'
