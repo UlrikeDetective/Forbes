e579e779beb12f2df07652650e86ff55b1094cc9
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2024-12-31 21:59:39 +0100

    raw data 12-2024

--- Forbes/forbes_sql/Forbes_modifyEntries.sql
+++ Forbes/forbes_sql/Forbes_modifyEntries.sql
@@ -2,6 +2,8 @@ Use Forbes;
 
 SELECT * FROM forbes_names WHERE Person = 'Taylor Swift';
 SELECT * FROM forbes_names WHERE Person = 'Susanne Klatten';
+SELECT * FROM forbes_names WHERE Person = 'Isak Andic';
+SELECT * FROM forbes_2024_11 WHERE Person = 'Isak Andic';
 
 UPDATE forbes_names
 SET City = 'Nashville', State = 'Tennessee', Citzenship = 'US', Self_made = 'TRUE'
