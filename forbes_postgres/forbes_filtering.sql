a5a337e674e8b84a66b75644379861bd377e1965
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2025-01-03 21:52:43 +0100

    Rewrite queries to Postgres

---
+++ forbes_postgres/forbes_filtering.sql
@@ -0,0 +1,36 @@
+\c forbes;
+
+SELECT User_ID, Person, Age_of_person_2023
+FROM forbes_2023
+WHERE Net_worth_in_BillionUSD_2023 > 75 AND Age_of_person_2023 < 50;
+
+SELECT User_ID, Person, Age_of_person_2023
+FROM forbes_2023
+WHERE Net_worth_in_BillionUSD_2023 > 75 OR Age_of_person_2023 < 50;
+
+SELECT User_ID, Person, Age_of_person_2024_08
+FROM forbes_2024_08
+WHERE Net_worth_in_BillionUSD_2024_08 > 75 AND Age_of_person_2024_08 < 50;
+
+SELECT User_ID, Person, Age_of_person_2024_10
+FROM forbes_2024_10
+WHERE Net_worth_in_BillionUSD_2024_10 > 75 AND Age_of_person_2024_10 < 40;
+
+SELECT User_ID, Person, Age_of_person_2022
+FROM forbes_2022
+WHERE Net_worth_in_BillionUSD_2022 > 75 OR Age_of_person_2022 < 50;
+
+SELECT Country_of_residence, COUNT(*) AS itemCount
+FROM forbes_names
+GROUP BY Country_of_residence
+HAVING COUNT(*) > 25;
+
+SELECT City, COUNT(*) AS itemCount
+FROM forbes_names
+GROUP BY City
+HAVING COUNT(*) > 15;
+
+SELECT Citizenship, COUNT(*) AS itemCount
+FROM forbes_names
+GROUP BY Citizenship
+HAVING COUNT(*) > 15;
---
+++ forbes_postgres/forbes_queries.sql
@@ -0,0 +1,243 @@
+\c forbes;
+
+SELECT 
+    forbes_names.Person,
+    forbes_names.ID,
+    forbes_names.Country_of_residence,
+    forbes_2021.net_worth_in_BillionUSD_2021,
+    forbes_2022.net_worth_in_BillionUSD_2022,
+    forbes_2023.net_worth_in_BillionUSD_2023,
+    forbes_2024_01.net_worth_in_BillionUSD_2024_01,
+    forbes_2024_02.net_worth_in_BillionUSD_2024_02,
+    forbes_2024_03.net_worth_in_BillionUSD_2024_03,
+    forbes_2024_04.net_worth_in_BillionUSD_2024_04,
+    forbes_2024_05.net_worth_in_BillionUSD_2024_05,
+    forbes_2024_06.net_worth_in_BillionUSD_2024_06,
+    forbes_2024_07.net_worth_in_BillionUSD_2024_07,
+    forbes_2024_08.net_worth_in_BillionUSD_2024_08,
+    forbes_2024_09.net_worth_in_BillionUSD_2024_09,
+    forbes_2024_10.net_worth_in_BillionUSD_2024_10
+FROM
+    forbes_names
+    LEFT JOIN forbes_2021 ON forbes_names.ID = forbes_2021.USER_ID
+    LEFT JOIN forbes_2022 ON forbes_names.ID = forbes_2022.USER_ID
+    LEFT JOIN forbes_2023 ON forbes_names.ID = forbes_2023.USER_ID
+    LEFT JOIN forbes_2024_01 ON forbes_names.ID = forbes_2024_01.USER_ID
+    LEFT JOIN forbes_2024_02 ON forbes_names.ID = forbes_2024_02.USER_ID
+    LEFT JOIN forbes_2024_03 ON forbes_names.ID = forbes_2024_03.USER_ID
+    LEFT JOIN forbes_2024_04 ON forbes_names.ID = forbes_2024_04.USER_ID
+    LEFT JOIN forbes_2024_05 ON forbes_names.ID = forbes_2024_05.USER_ID
+    LEFT JOIN forbes_2024_06 ON forbes_names.ID = forbes_2024_06.USER_ID
+    LEFT JOIN forbes_2024_07 ON forbes_names.ID = forbes_2024_07.USER_ID
+    LEFT JOIN forbes_2024_08 ON forbes_names.ID = forbes_2024_08.USER_ID
+    LEFT JOIN forbes_2024_09 ON forbes_names.ID = forbes_2024_09.USER_ID
+    LEFT JOIN forbes_2024_10 ON forbes_names.ID = forbes_2024_10.USER_ID
+WHERE forbes_names.Person = 'Bill Gates';
+
+SELECT 
+    forbes_names.Person,
+    forbes_names.ID,
+    forbes_names.Country_of_residence,
+    forbes_2021.net_worth_in_BillionUSD_2021,
+    forbes_2022.net_worth_in_BillionUSD_2022,
+    forbes_2023.net_worth_in_BillionUSD_2023,
+    forbes_2024_01.net_worth_in_BillionUSD_2024_01,
+    forbes_2024_02.net_worth_in_BillionUSD_2024_02,
+    forbes_2024_03.net_worth_in_BillionUSD_2024_03,
+    forbes_2024_04.net_worth_in_BillionUSD_2024_04,
+    forbes_2024_05.net_worth_in_BillionUSD_2024_05,
+    forbes_2024_06.net_worth_in_BillionUSD_2024_06,
+    forbes_2024_07.net_worth_in_BillionUSD_2024_07,
+    forbes_2024_08.net_worth_in_BillionUSD_2024_08,
+    forbes_2024_09.net_worth_in_BillionUSD_2024_09,
+    forbes_2024_10.net_worth_in_BillionUSD_2024_10
+FROM
+    forbes_names
+    LEFT JOIN forbes_2021 ON forbes_names.ID = forbes_2021.USER_ID
+    LEFT JOIN forbes_2022 ON forbes_names.ID = forbes_2022.USER_ID
+    LEFT JOIN forbes_2023 ON forbes_names.ID = forbes_2023.USER_ID
+    LEFT JOIN forbes_2024_01 ON forbes_names.ID = forbes_2024_01.USER_ID
+    LEFT JOIN forbes_2024_02 ON forbes_names.ID = forbes_2024_02.USER_ID
+    LEFT JOIN forbes_2024_03 ON forbes_names.ID = forbes_2024_03.USER_ID
+    LEFT JOIN forbes_2024_04 ON forbes_names.ID = forbes_2024_04.USER_ID
+    LEFT JOIN forbes_2024_05 ON forbes_names.ID = forbes_2024_05.USER_ID
+    LEFT JOIN forbes_2024_06 ON forbes_names.ID = forbes_2024_06.USER_ID
+    LEFT JOIN forbes_2024_07 ON forbes_names.ID = forbes_2024_07.USER_ID
+    LEFT JOIN forbes_2024_08 ON forbes_names.ID = forbes_2024_08.USER_ID
+    LEFT JOIN forbes_2024_09 ON forbes_names.ID = forbes_2024_09.USER_ID
+    LEFT JOIN forbes_2024_10 ON forbes_names.ID = forbes_2024_10.USER_ID
+WHERE forbes_names.Person LIKE 'Bill%'
+ORDER BY forbes_2022.net_worth_in_BillionUSD_2022 DESC;
+
+SELECT 
+    forbes_names.Person,
+    forbes_names.ID,
+    forbes_names.Country_of_residence,
+    forbes_2021.net_worth_in_BillionUSD_2021,
+    forbes_2022.net_worth_in_BillionUSD_2022,
+    forbes_2023.net_worth_in_BillionUSD_2023,
+    forbes_2024_01.net_worth_in_BillionUSD_2024_01,
+    forbes_2024_02.net_worth_in_BillionUSD_2024_02,
+    forbes_2024_03.net_worth_in_BillionUSD_2024_03,
+    forbes_2024_04.net_worth_in_BillionUSD_2024_04,
+    forbes_2024_05.net_worth_in_BillionUSD_2024_05,
+    forbes_2024_06.net_worth_in_BillionUSD_2024_06,
+    forbes_2024_07.net_worth_in_BillionUSD_2024_07,
+    forbes_2024_08.net_worth_in_BillionUSD_2024_08,
+    forbes_2024_09.net_worth_in_BillionUSD_2024_09,
+    forbes_2024_10.net_worth_in_BillionUSD_2024_10
+FROM
+    forbes_names
+    LEFT JOIN forbes_2021 ON forbes_names.ID = forbes_2021.USER_ID
+    LEFT JOIN forbes_2022 ON forbes_names.ID = forbes_2022.USER_ID
+    LEFT JOIN forbes_2023 ON forbes_names.ID = forbes_2023.USER_ID
+    LEFT JOIN forbes_2024_01 ON forbes_names.ID = forbes_2024_01.USER_ID
+    LEFT JOIN forbes_2024_02 ON forbes_names.ID = forbes_2024_02.USER_ID
+    LEFT JOIN forbes_2024_03 ON forbes_names.ID = forbes_2024_03.USER_ID
+    LEFT JOIN forbes_2024_04 ON forbes_names.ID = forbes_2024_04.USER_ID
+    LEFT JOIN forbes_2024_05 ON forbes_names.ID = forbes_2024_05.USER_ID
+    LEFT JOIN forbes_2024_06 ON forbes_names.ID = forbes_2024_06.USER_ID
+    LEFT JOIN forbes_2024_07 ON forbes_names.ID = forbes_2024_07.USER_ID
+    LEFT JOIN forbes_2024_08 ON forbes_names.ID = forbes_2024_08.USER_ID
+    LEFT JOIN forbes_2024_09 ON forbes_names.ID = forbes_2024_09.USER_ID
+    LEFT JOIN forbes_2024_10 ON forbes_names.ID = forbes_2024_10.USER_ID
+WHERE forbes_names.Person LIKE 'Steve%'
+ORDER BY forbes_2022.net_worth_in_BillionUSD_2022 DESC;
+
+SELECT 
+    forbes_names.Person,
+    forbes_names.ID,
+    forbes_names.Country_of_residence,
+    forbes_2021.net_worth_in_BillionUSD_2021,
+    forbes_2022.net_worth_in_BillionUSD_2022,
+    forbes_2023.net_worth_in_BillionUSD_2023,
+    forbes_2024_01.net_worth_in_BillionUSD_2024_01,
+    forbes_2024_02.net_worth_in_BillionUSD_2024_02,
+    forbes_2024_03.net_worth_in_BillionUSD_2024_03,
+    forbes_2024_04.net_worth_in_BillionUSD_2024_04,
+    forbes_2024_05.net_worth_in_BillionUSD_2024_05,
+    forbes_2024_06.net_worth_in_BillionUSD_2024_06,
+    forbes_2024_07.net_worth_in_BillionUSD_2024_07,
+    forbes_2024_08.net_worth_in_BillionUSD_2024_08,
+    forbes_2024_09.net_worth_in_BillionUSD_2024_09,
+    forbes_2024_10.net_worth_in_BillionUSD_2024_10
+FROM
+    forbes_names
+    LEFT JOIN forbes_2021 ON forbes_names.ID = forbes_2021.USER_ID
+    LEFT JOIN forbes_2022 ON forbes_names.ID = forbes_2022.USER_ID
+    LEFT JOIN forbes_2023 ON forbes_names.ID = forbes_2023.USER_ID
+    LEFT JOIN forbes_2024_01 ON forbes_names.ID = forbes_2024_01.USER_ID
+    LEFT JOIN forbes_2024_02 ON forbes_names.ID = forbes_2024_02.USER_ID
+    LEFT JOIN forbes_2024_03 ON forbes_names.ID = forbes_2024_03.USER_ID
+    LEFT JOIN forbes_2024_04 ON forbes_names.ID = forbes_2024_04.USER_ID
+    LEFT JOIN forbes_2024_05 ON forbes_names.ID = forbes_2024_05.USER_ID
+    LEFT JOIN forbes_2024_06 ON forbes_names.ID = forbes_2024_06.USER_ID
+    LEFT JOIN forbes_2024_07 ON forbes_names.ID = forbes_2024_07.USER_ID
+    LEFT JOIN forbes_2024_08 ON forbes_names.ID = forbes_2024_08.USER_ID
+    LEFT JOIN forbes_2024_09 ON forbes_names.ID = forbes_2024_09.USER_ID
+    LEFT JOIN forbes_2024_10 ON forbes_names.ID = forbes_2024_10.USER_ID
+WHERE forbes_names.Person LIKE 'Ulrike%';
+
+SELECT 
+    forbes_names.Person,
+    forbes_names.ID,
+    forbes_names.City,
+    forbes_2021.net_worth_in_BillionUSD_2021,
+    forbes_2022.net_worth_in_BillionUSD_2022,
+    forbes_2023.net_worth_in_BillionUSD_2023,
+    forbes_2024_01.net_worth_in_BillionUSD_2024_01,
+    forbes_2024_02.net_worth_in_BillionUSD_2024_02,
+    forbes_2024_03.net_worth_in_BillionUSD_2024_03,
+    forbes_2024_04.net_worth_in_BillionUSD_2024_04,
+    forbes_2024_05.net_worth_in_BillionUSD_2024_05,
+    forbes_2024_06.net_worth_in_BillionUSD_2024_06,
+    forbes_2024_07.net_worth_in_BillionUSD_2024_07,
+    forbes_2024_08.net_worth_in_BillionUSD_2024_08,
+    forbes_2024_09.net_worth_in_BillionUSD_2024_09,
+    forbes_2024_10.net_worth_in_BillionUSD_2024_10
+FROM
+    forbes_names
+    LEFT JOIN forbes_2021 ON forbes_names.ID = forbes_2021.USER_ID
+    LEFT JOIN forbes_2022 ON forbes_names.ID = forbes_2022.USER_ID
+    LEFT JOIN forbes_2023 ON forbes_names.ID = forbes_2023.USER_ID
+    LEFT JOIN forbes_2024_01 ON forbes_names.ID = forbes_2024_01.USER_ID
+    LEFT JOIN forbes_2024_02 ON forbes_names.ID = forbes_2024_02.USER_ID
+    LEFT JOIN forbes_2024_03 ON forbes_names.ID = forbes_2024_03.USER_ID
+    LEFT JOIN forbes_2024_04 ON forbes_names.ID = forbes_2024_04.USER_ID
+    LEFT JOIN forbes_2024_05 ON forbes_names.ID = forbes_2024_05.USER_ID
+    LEFT JOIN forbes_2024_06 ON forbes_names.ID = forbes_2024_06.USER_ID
+    LEFT JOIN forbes_2024_07 ON forbes_names.ID = forbes_2024_07.USER_ID
+    LEFT JOIN forbes_2024_08 ON forbes_names.ID = forbes_2024_08.USER_ID
+    LEFT JOIN forbes_2024_09 ON forbes_names.ID = forbes_2024_09.USER_ID
+    LEFT JOIN forbes_2024_10 ON forbes_names.ID = forbes_2024_10.USER_ID
+WHERE forbes_names.City LIKE 'Paris'
+ORDER BY forbes_2022.net_worth_in_BillionUSD_2022 DESC;
+
+SELECT 
+    forbes_names.Person,
+    forbes_names.ID,
+    forbes_names.City,
+    forbes_2021.net_worth_in_BillionUSD_2021,
+    forbes_2022.net_worth_in_BillionUSD_2022,
+    forbes_2023.net_worth_in_BillionUSD_2023,
+    forbes_2024_01.net_worth_in_BillionUSD_2024_01,
+    forbes_2024_02.net_worth_in_BillionUSD_2024_02,
+    forbes_2024_03.net_worth_in_BillionUSD_2024_03,
+    forbes_2024_04.net_worth_in_BillionUSD_2024_04,
+    forbes_2024_05.net_worth_in_BillionUSD_2024_05,
+    forbes_2024_06.net_worth_in_BillionUSD_2024_06,
+    forbes_2024_07.net_worth_in_BillionUSD_2024_07,
+    forbes_2024_08.net_worth_in_BillionUSD_2024_08,
+    forbes_2024_09.net_worth_in_BillionUSD_2024_09,
+    forbes_2024_10.net_worth_in_BillionUSD_2024_10
+FROM
+    forbes_names
+    LEFT JOIN forbes_2021 ON forbes_names.ID = forbes_2021.USER_ID
+    LEFT JOIN forbes_2022 ON forbes_names.ID = forbes_2022.USER_ID
+    LEFT JOIN forbes_2023 ON forbes_names.ID = forbes_2023.USER_ID
+    LEFT JOIN forbes_2024_01 ON forbes_names.ID = forbes_2024_01.USER_ID
+    LEFT JOIN forbes_2024_02 ON forbes_names.ID = forbes_2024_02.USER_ID
+    LEFT JOIN forbes_2024_03 ON forbes_names.ID = forbes_2024_03.USER_ID
+    LEFT JOIN forbes_2024_04 ON forbes_names.ID = forbes_2024_04.USER_ID
+    LEFT JOIN forbes_2024_05 ON forbes_names.ID = forbes_2024_05.USER_ID
+    LEFT JOIN forbes_2024_06 ON forbes_names.ID = forbes_2024_06.USER_ID
+    LEFT JOIN forbes_2024_07 ON forbes_names.ID = forbes_2024_07.USER_ID
+    LEFT JOIN forbes_2024_08 ON forbes_names.ID = forbes_2024_08.USER_ID
+    LEFT JOIN forbes_2024_09 ON forbes_names.ID = forbes_2024_09.USER_ID
+    LEFT JOIN forbes_2024_10 ON forbes_names.ID = forbes_2024_10.USER_ID
+WHERE forbes_names.City LIKE 'Leipzig'
+ORDER BY forbes_2022.net_worth_in_BillionUSD_2022 DESC;
+
+SELECT 
+    forbes_names.Person,
+    forbes_names.ID,
+    forbes_names.Country_of_residence,
+    forbes_2022.net_worth_in_BillionUSD_2022,
+    forbes_2023.net_worth_in_BillionUSD_2023,
+    forbes_2024_01.net_worth_in_BillionUSD_2024_01,
+    forbes_2024_02.net_worth_in_BillionUSD_2024_02,
+    forbes_2024_03.net_worth_in_BillionUSD_2024_03,
+    forbes_2024_04.net_worth_in_BillionUSD_2024_04,
+    forbes_2024_05.net_worth_in_BillionUSD_2024_05,
+    forbes_2024_06.net_worth_in_BillionUSD_2024_06,
+    forbes_2024_07.net_worth_in_BillionUSD_2024_07,
+    forbes_2024_08.net_worth_in_BillionUSD_2024_08,
+    forbes_2024_09.net_worth_in_BillionUSD_2024_09,
+    forbes_2024_10.net_worth_in_BillionUSD_2024_10
+FROM
+    forbes_names
+    LEFT JOIN forbes_2021 ON forbes_names.ID = forbes_2021.USER_ID
+    LEFT JOIN forbes_2022 ON forbes_names.ID = forbes_2022.USER_ID
+    LEFT JOIN forbes_2023 ON forbes_names.ID = forbes_2023.USER_ID
+    LEFT JOIN forbes_2024_01 ON forbes_names.ID = forbes_2024_01.USER_ID
+    LEFT JOIN forbes_2024_02 ON forbes_names.ID = forbes_2024_02.USER_ID
+    LEFT JOIN forbes_2024_03 ON forbes_names.ID = forbes_2024_03.USER_ID
+    LEFT JOIN forbes_2024_04 ON forbes_names.ID = forbes_2024_04.USER_ID
+    LEFT JOIN forbes_2024_05 ON forbes_names.ID = forbes_2024_05.USER_ID
+    LEFT JOIN forbes_2024_06 ON forbes_names.ID = forbes_2024_06.USER_ID
+    LEFT JOIN forbes_2024_07 ON forbes_names.ID = forbes_2024_07.USER_ID
+    LEFT JOIN forbes_2024_08 ON forbes_names.ID = forbes_2024_08.USER_ID
+    LEFT JOIN forbes_2024_09 ON forbes_names.ID = forbes_2024_09.USER_ID
+    LEFT JOIN forbes_2024_10 ON forbes_names.ID = forbes_2024_10.USER_ID
+WHERE forbes_names.Country_of_residence = 'Germany'
+ORDER BY forbes_2022.net_worth_in_BillionUSD_2022 DESC;
